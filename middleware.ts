b1ff016a4419c821e44d79af2b7259224ed8c19b
iremaksoy14 <aksoyirem612@gmail.com>
2025-08-24 22:31:30 +0300

    middleware devre dışı bırakıldı

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,13 @@
-import createMiddleware from "next-intl/middleware";
+// import createMiddleware from "next-intl/middleware";
 
-export default createMiddleware({
-  // desteklenen diller
-  locales: ["tr", "en"],
-  defaultLocale: "tr", // varsayılan
-  localePrefix: "always", // URL: /tr, /en ...
-});
+// export default createMiddleware({
+//   // desteklenen diller
+//   locales: ["tr", "en"],
+//   defaultLocale: "tr", // varsayılan
+//   localePrefix: "always", // URL: /tr, /en ...
+// });
 
-// public dosyalar ve next içi rotaları hariç tut
-export const config = {
-  matcher: ["/((?!api|_next|.*\\..*).*)"],
-};
+// // public dosyalar ve next içi rotaları hariç tut
+// export const config = {
+//   matcher: ["/((?!api|_next|.*\\..*).*)"],
+// };
